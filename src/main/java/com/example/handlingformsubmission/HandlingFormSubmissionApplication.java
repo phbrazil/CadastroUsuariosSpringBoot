53bc9afecbb13cfca455d2434e85f17d3a6cd076
Paulo Bezerra <paulo.bezerra@mci-group.com>
2020-05-12 12:34:24 -0300

    commit 1

--- src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
+++ src/main/java/com/example/handlingformsubmission/HandlingFormSubmissionApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HandlingFormSubmissionApplication extends SpringBootServletInitializer{
+//public class HandlingFormSubmissionApplication extends SpringBootServletInitializer{
+public class HandlingFormSubmissionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HandlingFormSubmissionApplication.class, args);
